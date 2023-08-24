35d80b2a55f884340493bec7a776da0680e91ea4
alexjsanchez1020 <alexjsanchez1020@gmail.com>
2023-08-24 05:52:49 -0400

    Post register returns proper object

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -10,8 +10,12 @@ router.post("/register", async (req, res, next) => {
     const hash = bcrypt.hashSync(password, 8);
     const newUser = { username, password: hash };
     const result = await authModel.add(newUser);
-    res.status(201).json({ message: `nice to have you ${username}` });
-    console.log(result);
+
+    res.status(201).json({
+      id: result.id,
+      username: result.username,
+      password: result.password,
+    });
   } catch (err) {
     next(err);
   }
