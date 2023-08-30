776dc7e06cf855cedea7e875763ba7b0043145f9
alexjsanchez1020 <alexjsanchez1020@gmail.com>
2023-08-30 05:46:02 -0400

    working on test

--- api/server.test.js
+++ api/server.test.js
@@ -3,6 +3,8 @@ const authModel = require("./auth/auth-model");
 const db = require("../data/dbConfig");
 const server = require("./server");
 const request = require("supertest");
+const jokes = require("./jokes/jokes-data");
+console.log(jokes);
 
 //before all we want to reset our data so where back start with a pure database
 
@@ -14,19 +16,31 @@ beforeAll(async () => {
 test("sanity", () => {
   expect(true).toBe(true);
 });
-
-describe("test2", () => {
-  test("check auth model", async () => {
+//first endpont
+describe("[GET] server", () => {
+  test("check server", async () => {
     const expectedCode = 200;
     const response = await request(server).get("/");
     expect(response.status).toEqual(expectedCode);
   });
+  test("makes sure api is running", async () => {
+    const expectedResponse = "running";
+    const response = await request(server).get("/");
+    expect(response.body.api).toBe(expectedResponse);
+  });
 });
-
+//second end point
 describe("[Get] /jokes", () => {
   test("responds with 200 Ok", async () => {
-    const res = await request(server).get("/api/jokes");
+    const res = await request(server).get("/api/jokes/");
     expect(res.status).toBe(200);
+  });
+  test("invalid token response", async () => {
+    const res = await request(server).get("/api/jokes/");
+    expect(res.body).toMatchObject({
+      message: "token required",
+      status: 401,
+    });
   });
 });
 
