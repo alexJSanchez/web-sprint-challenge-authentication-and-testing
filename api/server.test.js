830afa0445cf1e6f4812f0607d1e2259bddc11a3
alexjsanchez1020 <alexjsanchez1020@gmail.com>
2023-08-30 03:47:42 -0400

    test jokes end point

--- api/server.test.js
+++ api/server.test.js
@@ -4,13 +4,36 @@ const db = require("../data/dbConfig");
 const server = require("./server");
 const request = require("supertest");
 
+//before all we want to reset our data so where back start with a pure database
+
+beforeAll(async () => {
+  await db.migrate.rollback();
+  await db.migrate.latest();
+});
+
 test("sanity", () => {
   expect(true).toBe(true);
 });
 
 describe("test2", () => {
   test("check auth model", async () => {
-    const response = await request(server).post("auth/register");
-    expect(response.status).toBe(201);
+    const expectedCode = 200;
+    const response = await request(server).get("/");
+    expect(response.status).toEqual(expectedCode);
   });
 });
+
+describe("[Get] /jokes", () => {
+  test("responds with 200 Ok", async () => {
+    const res = await request(server).get("/api/jokes");
+    expect(res.status).toBe(200);
+  });
+});
+
+// describe("test3", () => {
+//   it("check if username is added", async () => {
+//     await authModel.add({ username: "player1", password: "hello" });
+//     const user = await db("users");
+//     expect(user).toHaveLength(1);
+//   });
+// });
